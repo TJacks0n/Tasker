6660601e53596595552791228a0bd0b6c287daee
Thomas Jackson <154314759+TJacks0n@users.noreply.github.com>
2025-04-24 20:11:17 +0100

    Initial Commit

---
+++ Tasker/TaskerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskerApp.swift
+//  Tasker
+//
+//  Created by Thomas Jackson on 24/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct TaskerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
